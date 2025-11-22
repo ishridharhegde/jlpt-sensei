93219dd786ea00050f6b63ee9a108bd4c304655d
Shridhar Hegde <shridhar.hegde@checkpt.com>
2025-11-22 22:19:54 +0530

    Hosting build files

--- test_unlimited.js
+++
@@ -1,5 +0,0 @@
-// Quick test to see what's happening
-import { getUnlimitedReviews } from './src/services/configService.js';
-
-console.log('Unlimited Reviews:', getUnlimitedReviews());
-console.log('Expected maxCards:', getUnlimitedReviews() ? Infinity : 20);
